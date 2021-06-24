dfe3be2fd29a096b89c4b30980016bebd0dfe20e
Alex <alexcheung880@gmail.com>
2021-06-23 20:23:51 -0700

    console log able to get alarm fired notification

---
+++ break.js
@@ -0,0 +1,27 @@
+// timer for eye break
+
+// get minutes to set timer
+document.getElementById('start').onclick = function() {
+    var timer = (parseInt(document.getElementById('minutes').value) * 60)
+}
+
+// alarm info
+let alarmInfo = {}
+//    specific time to fire alarm
+//    when: Date.now() + timer
+    // delay X minutes then fire alarm
+//    delayInMinutes: 1,
+    // fire alarm every 60 minutes for 10 times
+
+alarmInfo.periodInMinutes = (1);
+
+
+// create the alarm
+chrome.alarms.create("my alarm", alarmInfo);
+
+// firing the alarm
+chrome.alarms.onAlarm.addListener(onAlarm);
+
+function onAlarm(alarm) {
+    console.log("Alarm Fired", alarm)
+}
